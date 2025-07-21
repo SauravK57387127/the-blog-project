83f58c2cb544b9e743a436a56e8871692010d961
SauravK57387127 <ksauravyadav123@gmail.com>
2025-07-21 21:47:45 +0530

    fix: add root and health check routes for railway deployment

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -7,6 +7,10 @@ import httpLogger from '../../../packages/logger/httpLogger.js';
 import routes from './routes/index.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.middleware.js';
 import { errorHander } from './middlewares/errorHandler.middleware.js';
+import { sendResponse } from './utils/sendResponse.js';
+import { asyncHandler } from './utils/asyncHandler.js';
+
+
 
 
 export const createApp = () => {
@@ -20,7 +24,29 @@ export const createApp = () => {
     app.use(httpLogger);
 
     app.use('/api', routes);
+
     
+    app.get(
+        '/health',
+        asyncHandler(async (req, res) => {
+            sendResponse({
+            res,
+            message: 'Service is healthy',
+            data: { timestamp: new Date() },
+            });
+        })
+    );
+
+    app.get(
+        '/',
+        asyncHandler(async (req, res) => {
+            sendResponse({
+            res,
+            message: '🚀 Backend is alive and running on Railway!',
+            });
+        })
+    );
+        
     app.use(notFoundHandler);
     app.use(errorHander);
 
