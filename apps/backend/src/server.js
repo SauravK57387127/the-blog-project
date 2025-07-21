7ac1bf17582022530484b04e5d2b59f42e833e32
SauravK57387127 <ksauravyadav123@gmail.com>
2025-07-21 22:21:54 +0530

    test: checking if any request even hitting server

--- apps/backend/src/server.js
+++ apps/backend/src/server.js
@@ -18,6 +18,11 @@ console.log("✅ App setup starting...");
 const app = createApp();
 // app.listen(port, () => logger.info(`🚀 Server running on port ${port}`));
 
+app.use('*', (req, res) => {
+  res.status(404).send(`❌ Route ${req.originalUrl} not found`);
+});
+
+
 app.get('/debug', (req, res) => {
   res.send('✅ Server reached debug route');
 });
