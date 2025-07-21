c3e971a407e784697f4858c22d2737dd7f0f88da
SauravK57387127 <ksauravyadav123@gmail.com>
2025-07-21 18:05:54 +0530

    chore: add REDIS_URL to .env and use in redis client

--- database/redis/redisClient.js
+++ database/redis/redisClient.js
@@ -1,6 +1,6 @@
 import Redis from 'ioredis';
 
 
-const redis = new Redis(); // defaults to localhost:6379
+const redis = new Redis(process.env.REDIS_URL); // defaults to localhost:6379 | even without URL
 
 export default redis;
