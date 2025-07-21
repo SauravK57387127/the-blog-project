5d9014812ae5a9a4607ef2aa6983378f108d81d2
SauravK57387127 <ksauravyadav123@gmail.com>
2025-07-21 19:47:25 +0530

    fix: resolve Redis ENOTFOUND by enabling dual-stack DNS

--- database/redis/redisClient.js
+++ database/redis/redisClient.js
@@ -1,6 +1,6 @@
 import Redis from 'ioredis';
 
 
-const redis = new Redis(process.env.REDIS_URL); // defaults to localhost:6379 | even without URL
+const redis = new Redis(process.env.REDIS_URL + '?family=0'); // defaults to localhost:6379 | even without URL
 
 export default redis;
