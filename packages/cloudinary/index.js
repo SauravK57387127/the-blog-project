a57ca23d53e18a286fd2722e553bf647fe1c5808
SauravK57387127 <ksauravyadav123@gmail.com>
2026-03-10 18:42:12 +0530

    fix(cloudinary): remove duplicate config import

--- packages/cloudinary/index.js
+++ packages/cloudinary/index.js
@@ -1,6 +1,5 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { config } from '@theblogproj/config';
-import { config } from '@theblogproj/config';
 
 // Configure Cloudinary
 cloudinary.config({
